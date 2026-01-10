842c0c9ddafe6596690e4f94b0e3e22504135cab
ThanSoe <89002719+ThanSoe16@users.noreply.github.com>
2026-01-10 18:38:46 +0700

    theme provider

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,12 +1,12 @@
-import Image from 'next/image';
 import HomeBanner from './_components/HomeBanner';
 import { Button } from '@/components/ui/button';
+import { Flex } from '@radix-ui/themes';
 
 export default function HomePage() {
   return (
-    <div className="flex flex-col items-center justify-center">
+    <Flex direction="column" align="center" justify="center" className="h-screen gap-4">
       <HomeBanner />
       <Button>Button</Button>
-    </div>
+    </Flex>
   );
 }
