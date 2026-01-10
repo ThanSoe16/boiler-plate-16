4ea260c2cbebd96a118a82c74029e29eadab267f
ThanSoe <89002719+ThanSoe16@users.noreply.github.com>
2026-01-10 18:41:35 +0700

    theme toggle

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,3 +1,4 @@
+import { ThemeToggle } from '@/components/shared/theme-toggle';
 import HomeBanner from './_components/HomeBanner';
 import { Button } from '@/components/ui/button';
 import { Flex } from '@radix-ui/themes';
@@ -5,6 +6,7 @@ import { Flex } from '@radix-ui/themes';
 export default function HomePage() {
   return (
     <Flex direction="column" align="center" justify="center" className="h-screen gap-4">
+      <ThemeToggle />
       <HomeBanner />
       <Button>Button</Button>
     </Flex>
